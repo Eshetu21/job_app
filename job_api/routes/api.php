a021dcd818dd7b9b461e6b615ad7238d2ecf04b5
eyob2m <eyob2etemariam@gmail.com>
2024-08-14 01:45:07 +0300

    some changes

--- job_api/routes/api.php
+++ job_api/routes/api.php
@@ -21,25 +21,13 @@ Route::post('/admin/register', [AdminController::class, 'register']);
 //Profiles
 Route::get("profile", [UserController::class, "getuserprofile"])->middleware("auth:sanctum");
 
-// User +
-Route::post('register', [UserController::class, "register"]);
-Route::post('login', [UserController::class, "login"]);
-Route::put('update', [UserController::class, "update"])->middleware("auth:sanctum");
-Route::delete('delete', [UserController::class, "delete"])->middleware("auth:sanctum");
 
-//JobSeeker
+//JobSeeker 
 Route::post('createjobseeker', [JobSeekerController::class, "createjobseeker"])->middleware("auth:sanctum");
 Route::get('showjobseeker', [JobSeekerController::class, "showjobseeker"])->middleware("auth:sanctum");
 Route::put('updatejobseeker/{id}', [JobSeekerController::class, "updatejobseeker"])->middleware("auth:sanctum");
 Route::delete('deletejobseeker/{id}', [JobSeekerController::class, "deletejobseeker"])->middleware("auth:sanctum");
 
-//PrivateClient
-Route::post('createprivateclient', [PrivateClientController::class, "createprivateclient"])->middleware("auth:sanctum");
-Route::post('privatecreatejob', [PrivateClientController::class, "privatecreatejob"])->middleware("auth:sanctum");
-
-//Company
-Route::post('createcompany', [CompanyController::class, 'createcompany'])->middleware("auth:sanctum");
-Route::post('companycreatejob', [CompanyController::class, 'companycreatejob'])->middleware("auth:sanctum");
 
 
 
@@ -58,3 +46,16 @@ Route::post('addexperience/{id}', [ExperienceController::class, 'addexperience']
 Route::get('showexperience', [ExperienceController::class, 'showexperience'])->middleware("auth:sanctum");;
 Route::put('updateeducation/{jobseekerid}/{educationid}', [EducationController::class, 'updateeducation'])->middleware("auth:sanctum");
 Route::post('deleteeducation/{id}', [EducationController::class, 'deleteeducation'])->middleware("auth:sanctum");
+
+// finalized
+// User +
+Route::post('register', [UserController::class, "register"]);
+Route::post('login', [UserController::class, "login"]);
+Route::put('update', [UserController::class, "update"])->middleware("auth:sanctum");
+Route::delete('delete', [UserController::class, "delete"])->middleware("auth:sanctum");
+//PrivateClient +
+Route::post('createprivateclient', [PrivateClientController::class, "createprivateclient"])->middleware("auth:sanctum");
+Route::post('privatecreatejob', [PrivateClientController::class, "privatecreatejob"])->middleware("auth:sanctum");
+//Company +
+Route::post('createcompany', [CompanyController::class, 'createcompany'])->middleware("auth:sanctum");
+Route::post('companycreatejob', [CompanyController::class, 'companycreatejob'])->middleware("auth:sanctum");
