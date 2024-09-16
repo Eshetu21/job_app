bee05862052c47e70abd60a3e3710d870cd4af82
eyob2m <eyob2etemariam@gmail.com>
2024-09-16 16:28:01 +0300

    fixes

--- job_api/resources/views/jobaccepted.blade.php
+++ job_api/resources/views/jobaccepted.blade.php
@@ -1,4 +1,13 @@
-<pre style="font-size: 13px;">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accepted</title>
+</head>
+<body>
+<div style="width: 100%;">
+   <pre style="font-size: 15px; font-family: 'Times New Roman', Times, serif; padding: 3px; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.4);">
 Dear <b>{{$username}}</b>,
 
 Congratulations! 🎉 You’ve been <span style="color: green;">accepted</span> to join <b>{{$companyname}}</b> as a <b>{{$jobtitle}}</b>. 
@@ -6,6 +15,14 @@ Please log in to the AddisJobs app to view your acceptance note and next steps.
 
 We’re excited to have you on board!
 
+{{config('constants.appname')}} Team
 Best regards,
 </pre>
+   </div>
+         
+         
+</body>
+</html>
+
+
 
--- job_api/resources/views/resetpassword.blade.php
+++ job_api/resources/views/resetpassword.blade.php
@@ -1,13 +1,26 @@
-<pre style="font-size: 13px;">
-Hi <b style="color: gray;">{{ $username }}</b>,
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset your password</title>
+</head>
+<body>
+   <div style="width: 100%;">
+   <pre style="font-size: 15px; font-family: 'Times New Roman', Times, serif; padding: 3px; box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.4);">
+    Hi <b style="color: gray;">{{ $username }}</b>,
 
 Your password reset code is: <b>{{ $pin }}</b>
 
-Enter this code in the Addis Jobs app to reset password.
-This code is valid for <b>10</b> minutes. If you didn't request this, please ignore this email.
+Enter this code in {{config('constants.appname')}} app to reset password. This code is valid for <b>5</b> minutes. If you didn't request this, please ignore this email.
 
-Best,
-Addis Jobs Team
+{{config('constants.appname')}} Team
 
 
 </pre>
+   </div>
+         
+         
+</body>
+</html>
